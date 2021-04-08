427ba98c32abf987e0ef5a631ab2ca781c309278
voduyquocthai <voduyquocthai@gmail.com>
2021-04-08 16:26:20 +0700

    added

---
+++ src/main/java/com/ryan/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.ryan.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
